5393ee6b3a1a55395befaab9b10ab972827f53fe
Saloni Shah <salonipshah11@gmail.com>
2022-07-26 17:23:46 -0400

    working code?

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,12 +1,13 @@
 import 'src/libs/routes'
 
 import { h } from 'react-hyperscript-helpers'
-import ReactNotification from 'react-notifications-component'
+import { ReactNotifications } from 'react-notifications-component'
 import { LocationProvider, Router } from 'src/libs/nav'
 
 
 const Main = () => {
   return h(LocationProvider, [
+    h(ReactNotifications),
     h(Router)
   ])
 }
