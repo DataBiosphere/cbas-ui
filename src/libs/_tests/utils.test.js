bfdd60653538124685933c935f3ec725d4b7fc62
Saloni Shah <salonipshah11@gmail.com>
2022-09-06 17:48:13 -0400

    only check date

--- src/libs/_tests/utils.test.js
+++ src/libs/_tests/utils.test.js
@@ -3,6 +3,8 @@ import { makeCompleteDate } from 'src/libs/utils'
 
 describe('makeCompleteDate', () => {
   it('converts string to Date', () => {
-    expect(makeCompleteDate('2022-01-27T22:27:15.591Z').toString()).toEqual('Jan 27, 2022, 5:27 PM')
+    // since the timestamp is being converted to Local timezone, it returns different time when run locally and in GitHub action.
+    // Hence verifying only the date format for now.
+    expect(makeCompleteDate('2022-01-27T22:27:15.591Z').toString()).toContain('Jan 27, 2022')
   })
 })
