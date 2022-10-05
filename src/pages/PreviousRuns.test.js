e1133d7122303dd8abc195a1c262c61cffe0cf31
Saloni Shah <salonipshah11@gmail.com>
2022-10-05 14:21:32 -0400

    add Last Changed column

--- src/pages/PreviousRuns.test.js
+++ src/pages/PreviousRuns.test.js
@@ -32,6 +32,7 @@ describe('Previous Runs page', () => {
         state: 'Submitted',
         submission_date: '2022-01-27T22:27:15.591Z',
         workflow_url: 'https://abc.wdl',
+        last_modified_timestamp: '2022-01-28T22:30:05.291Z',
         workflow_params: '[{"parameter_name":"workflow_input_foo","parameter_type":"String","source":{"type":"literal","entity_attribute":"helloworld"}},{"parameter_name":"workflow_input_foo_rating","parameter_type":"Int","source":{"type":"entity_lookup","entity_attribute":"entity_field_foo_rating"}}]'
       },
       {
@@ -39,6 +40,7 @@ describe('Previous Runs page', () => {
         state: 'Failed',
         submission_date: '2022-07-14T22:22:15.591Z',
         workflow_url: 'https://xyz.wdl',
+        last_modified_timestamp: '2022-07-14T23:14:25.791Z',
         workflow_params: '[{"parameter_name":"workflow_input_foo","parameter_type":"String","source":{"type":"literal","entity_attribute":"helloworld"}},{"parameter_name":"workflow_input_foo_rating","parameter_type":"Int","source":{"type":"entity_lookup","entity_attribute":"entity_field_foo_rating"}}]'
       }
     ]
@@ -93,7 +95,7 @@ describe('Previous Runs page', () => {
     expect(getRunsMethod).toBeCalledTimes(1)
 
     const table = screen.getByRole('table')
-    expect(table).toHaveAttribute('aria-colcount', '4')
+    expect(table).toHaveAttribute('aria-colcount', '5')
     expect(table).toHaveAttribute('aria-rowcount', '1')
   })
 
@@ -106,31 +108,34 @@ describe('Previous Runs page', () => {
     const table = screen.getByRole('table')
 
     // Assert
-    expect(table).toHaveAttribute('aria-colcount', '4')
+    expect(table).toHaveAttribute('aria-colcount', '5')
     expect(table).toHaveAttribute('aria-rowcount', '3')
 
     const rows = within(table).queryAllByRole('row')
     expect(rows.length).toBe(3)
 
     const headers = within(rows[0]).queryAllByRole('columnheader')
-    expect(headers.length).toBe(4)
+    expect(headers.length).toBe(5)
     within(headers[0]).getByText('Run ID')
     within(headers[1]).getByText('Status')
-    within(headers[2]).getByText('Inputs')
-    within(headers[3]).getByText('Submitted')
+    within(headers[2]).getByText('Last Changed')
+    within(headers[3]).getByText('Inputs')
+    within(headers[4]).getByText('Submitted')
 
     // check data rows are rendered as expected
     const cellsFromDataRow1 = within(rows[1]).queryAllByRole('cell')
-    expect(cellsFromDataRow1.length).toBe(4)
+    expect(cellsFromDataRow1.length).toBe(5)
     within(cellsFromDataRow1[0]).getByText('b7234aae-6f43-405e-bb3a-71f924e09825')
     within(cellsFromDataRow1[1]).getByText('Failed')
-    within(cellsFromDataRow1[3]).getByText(/Jul 14, 2022/)
+    within(cellsFromDataRow1[2]).getByText(/Jul 14, 2022/)
+    within(cellsFromDataRow1[4]).getByText(/Jul 14, 2022/)
 
     const cellsFromDataRow2 = within(rows[2]).queryAllByRole('cell')
-    expect(cellsFromDataRow2.length).toBe(4)
+    expect(cellsFromDataRow2.length).toBe(5)
     within(cellsFromDataRow2[0]).getByText('ea001565-1cd6-4e43-b446-932ac1918081')
     within(cellsFromDataRow2[1]).getByText('Submitted')
-    within(cellsFromDataRow2[3]).getByText(/Jan 27, 2022/)
+    within(cellsFromDataRow2[2]).getByText(/Jan 28, 2022/)
+    within(cellsFromDataRow2[4]).getByText(/Jan 27, 2022/)
   })
 
   it('should sort columns properly', async () => {
@@ -144,24 +149,26 @@ describe('Previous Runs page', () => {
     expect(rows.length).toBe(3)
 
     const headers = within(rows[0]).queryAllByRole('columnheader')
-    expect(headers.length).toBe(4)
+    expect(headers.length).toBe(5)
 
     await act(async () => {
-      await fireEvent.click(within(headers[3]).getByRole('button'))
+      await fireEvent.click(within(headers[4]).getByRole('button'))
     })
 
     // Assert - rows are now sorted by submission timestamp in ascending order
     const cellsFromUpdatedDataRow1 = within(rows[1]).queryAllByRole('cell')
-    expect(cellsFromUpdatedDataRow1.length).toBe(4)
+    expect(cellsFromUpdatedDataRow1.length).toBe(5)
     within(cellsFromUpdatedDataRow1[0]).getByText('ea001565-1cd6-4e43-b446-932ac1918081')
     within(cellsFromUpdatedDataRow1[1]).getByText('Submitted')
-    within(cellsFromUpdatedDataRow1[3]).getByText(/Jan 27, 2022/)
+    within(cellsFromUpdatedDataRow1[2]).getByText(/Jan 28, 2022/)
+    within(cellsFromUpdatedDataRow1[4]).getByText(/Jan 27, 2022/)
 
     const cellsFromUpdatedDataRow2 = within(rows[2]).queryAllByRole('cell')
-    expect(cellsFromUpdatedDataRow2.length).toBe(4)
+    expect(cellsFromUpdatedDataRow2.length).toBe(5)
     within(cellsFromUpdatedDataRow2[0]).getByText('b7234aae-6f43-405e-bb3a-71f924e09825')
     within(cellsFromUpdatedDataRow2[1]).getByText('Failed')
-    within(cellsFromUpdatedDataRow2[3]).getByText(/Jul 14, 2022/)
+    within(cellsFromUpdatedDataRow2[2]).getByText(/Jul 14, 2022/)
+    within(cellsFromUpdatedDataRow2[4]).getByText(/Jul 14, 2022/)
 
     // Act - click on sort button on Status column
     await act(async () => {
@@ -170,15 +177,17 @@ describe('Previous Runs page', () => {
 
     // Assert that sort by Status worked
     const updatedDataRow1Cells = within(rows[1]).queryAllByRole('cell')
-    expect(updatedDataRow1Cells.length).toBe(4)
+    expect(updatedDataRow1Cells.length).toBe(5)
     within(updatedDataRow1Cells[0]).getByText('b7234aae-6f43-405e-bb3a-71f924e09825')
     within(updatedDataRow1Cells[1]).getByText('Failed')
-    within(updatedDataRow1Cells[3]).getByText(/Jul 14, 2022/)
+    within(updatedDataRow1Cells[2]).getByText(/Jul 14, 2022/)
+    within(updatedDataRow1Cells[4]).getByText(/Jul 14, 2022/)
 
     const updatedDataRow2Cells = within(rows[2]).queryAllByRole('cell')
-    expect(updatedDataRow2Cells.length).toBe(4)
+    expect(updatedDataRow2Cells.length).toBe(5)
     within(updatedDataRow2Cells[0]).getByText('ea001565-1cd6-4e43-b446-932ac1918081')
     within(updatedDataRow2Cells[1]).getByText('Submitted')
-    within(updatedDataRow2Cells[3]).getByText(/Jan 27, 2022/)
+    within(updatedDataRow2Cells[2]).getByText(/Jan 28, 2022/)
+    within(updatedDataRow2Cells[4]).getByText(/Jan 27, 2022/)
   })
 })
