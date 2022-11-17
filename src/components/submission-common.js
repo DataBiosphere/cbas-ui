64d679d9b0514b1a075e6c7f1f095625a297898a
mspector <michael.l.spector@gmail.com>
2022-11-17 13:48:43 -0500

    add icons (#39)
    * add icons
    
    * fix variable name

---
+++ src/components/submission-common.js
@@ -0,0 +1,53 @@
+import { div } from 'react-hyperscript-helpers'
+import { icon } from 'src/components/icons'
+import colors from 'src/libs/colors'
+
+
+const iconSize = 24
+export const addCountSuffix = (label, count = undefined) => {
+  return label + (count === undefined ? '' : `: ${count}`)
+}
+
+export const statusType = {
+  succeeded: {
+    id: 'succeeded', // Must match variable name for collection unpacking.
+    label: () => 'Succeeded',
+    icon: style => icon('check', { size: iconSize, style: { color: colors.success(), ...style } })
+  },
+  failed: {
+    id: 'failed', // Must match variable name for collection unpacking.
+    label: () => 'Failed',
+    icon: style => icon('warning-standard', { size: iconSize, style: { color: colors.danger(), ...style } })
+  },
+  running: {
+    id: 'running', // Must match variable name for collection unpacking.
+    label: () => 'Running',
+    icon: style => icon('sync', { size: iconSize, style: { color: colors.dark(), ...style } })
+  },
+  submitted: {
+    id: 'submitted', // Must match variable name for collection unpacking.
+    label: () => 'Submitted',
+    icon: style => icon('clock', { size: iconSize, style: { color: colors.dark(), ...style } })
+  },
+  waitingForQuota: {
+    id: 'waitingForQuota', // Must match variable name for collection unpacking.
+    label: () => 'Submitted, Awaiting Cloud Quota',
+    icon: style => icon('error-standard', { size: iconSize, style: { color: colors.warning(), ...style } }),
+    moreInfoLink: 'https://support.terra.bio/hc/en-us/articles/360029071251',
+    moreInfoLabel: 'Learn more about cloud quota',
+    tooltip: 'Delayed by Google Cloud Platform (GCP) quota limits. Contact Terra Support to request a quota increase.'
+  },
+  unknown: {
+    id: 'unknown', // Must match variable name for collection unpacking.
+    label: executionStatus => `Unexpected status (${executionStatus})`,
+    icon: style => icon('question', { size: iconSize, style: { color: colors.dark(), ...style } })
+  }
+}
+
+/**
+ * Returns the rendered status line, based on icon function, label, and style.
+ */
+export const makeStatusLine = (iconFn, label, style) => div(
+  { style: { display: 'flex', alignItems: 'center', fontSize: 14, ...style } },
+  [iconFn({ marginRight: '0.5rem' }), label]
+)
