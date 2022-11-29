62dfe61a53f2fb4b69077fd005f6ed868e931758
Michael Spector <michael.l.spector@gmail.com>
2022-11-29 15:53:46 -0500

    tab selection is barebones functional

--- src/pages/SubmissionConfig.js
+++ src/pages/SubmissionConfig.js
@@ -1,17 +1,18 @@
 import { Fragment, useState } from 'react'
 import { a, div, h, h2, span } from 'react-hyperscript-helpers'
-import { ButtonPrimary, GroupedSelect, headerBar, Link } from 'src/components/common'
+import { ButtonPrimary, headerBar, Link, Select } from 'src/components/common'
 import StepButtons from 'src/components/StepButtons'
+import * as Utils from 'src/libs/utils'
 
 
 const wdlUrl = 'https://raw.githubusercontent.com/broadinstitute/viral-pipelines/master/pipes/WDL/workflows/fastq_to_ubam.wdl'
 
 export const SubmissionConfig = () => {
   const [activeTab, setActiveTab] = useState({ key: 'select-data' })
+  const [dataTable, setDatatable] = useState({ name: null })
 
-  return h(Fragment, [
-    headerBar(),
-    div({ style: { margin: '4em' } }, [
+  const renderSummary = () => {
+    return div({ style: { margin: '4em' } }, [
       div({ style: { display: 'flex', marginTop: '1rem', justifyContent: 'space-between' } }, [
         h2(['Fastq_to_ubam'])
       ]),
@@ -28,24 +29,20 @@ export const SubmissionConfig = () => {
           Link,
           {
             style: { textDecoration: 'underline' },
-            onClick: () => { window.alert('this will go to the Submission Details page') }
+            onClick: () => { window.alert('TODO: what happens when the user clicks this?') }
           },
           ['View Script']
         )
       ]),
       div({ style: { marginTop: '2rem', height: '2rem', fontWeight: 'bold' } }, ['Select a data table']),
-      h(GroupedSelect, {
-        'aria-label': 'Entity type selector',
+      h(Select, {
+        isDisabled: false,
+        'aria-label': 'Select a data table',
         isClearable: false,
-        // isDisabled: currentSnapRedacted || this.isSingle() || !!Utils.editWorkspaceError(ws),
-        isSearchable: true,
-        placeholder: 'None selected',
-        styles: { container: old => ({ ...old, display: 'inline-block', width: 200, marginLeft: '0.5rem' }) },
-        // value: selectedEntityType,
-        onChange: async () => {},
-        options: [
-          { label: 'Covid19_DataTable' }
-        ]
+        value: dataTable.name,
+        onChange: ({ value }) => setDatatable({ name: value }),
+        styles: {},
+        options: ['Covid19_DataTable', 'Some Other DataTable']
       }),
       h(StepButtons, {
         tabs: [
@@ -63,6 +60,28 @@ export const SubmissionConfig = () => {
         }, ['Submit'])
       })
     ])
+  }
+
+  const renderDataSelector = () => {
+    return 'data selector'
+  }
+
+  const renderInputs = () => {
+    return 'inputs'
+  }
+
+  const renderOutputs = () => {
+    return 'outputs'
+  }
+
+  return h(Fragment, [
+    headerBar(),
+    renderSummary(),
+    Utils.switchCase(activeTab.key || 'select-data',
+      ['select-data', () => renderDataSelector()],
+      ['inputs', () => renderInputs()],
+      ['outputs', () => renderOutputs()]
+    )
   ])
 }
 
