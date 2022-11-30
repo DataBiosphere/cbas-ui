7d71544af10b4e3ebbdd893be85db0b53f59b54b
mspector <michael.l.spector@gmail.com>
2022-11-30 09:57:30 -0500

    use mock data to populate SubmissionConfig info (#51)

--- src/pages/SubmissionConfig.js
+++ src/pages/SubmissionConfig.js
@@ -1,30 +1,62 @@
+import _ from 'lodash/fp'
 import { Fragment, useState } from 'react'
 import { a, div, h, h2, span } from 'react-hyperscript-helpers'
 import { ButtonPrimary, headerBar, Link, Select } from 'src/components/common'
 import StepButtons from 'src/components/StepButtons'
+import { Ajax } from 'src/libs/ajax'
+import { notify } from 'src/libs/notifications'
+import { useCancellation, useOnMount } from 'src/libs/react-utils'
 import * as Utils from 'src/libs/utils'
 
 
-const wdlUrl = 'https://raw.githubusercontent.com/broadinstitute/viral-pipelines/master/pipes/WDL/workflows/fastq_to_ubam.wdl'
+const methodId = '64b5bc5e-85cf-4aff-b522-01471b88b950' // TODO: get this from the previous page
+// const wdlUrl = 'https://raw.githubusercontent.com/broadinstitute/viral-pipelines/master/pipes/WDL/workflows/fastq_to_ubam.wdl'
 
 export const SubmissionConfig = () => {
   const [activeTab, setActiveTab] = useState({ key: 'select-data' })
-  const [dataTable, setDatatable] = useState({ name: null })
+  const [selectedTable, setSelectedTable] = useState({ name: null })
+  const [methodsData, setMethodsData] = useState({})
+  const [dataTables, setDataTables] = useState()
+
+  const signal = useCancellation()
+
+  useOnMount(() => {
+    const loadMethodsData = async () => {
+      try {
+        const allMethods = await Ajax(signal).Cbas.methods.get()
+        const selectedMethod = _.head(_.filter(m => m.method_id === methodId, allMethods))
+        setMethodsData(selectedMethod) // TODO: this doesn't feel "safe"
+      } catch (error) {
+        notify('error', 'Error loading methods data', { detail: await (error instanceof Response ? error.text() : error) })
+      }
+    }
+
+    const loadTablesData = async () => {
+      try {
+        setDataTables(await Ajax(signal).Wds.types.get())
+      } catch (error) {
+        notify('error', 'Error loading tables data', { detail: await (error instanceof Response ? error.text() : error) })
+      }
+    }
+
+    loadMethodsData()
+    loadTablesData()
+  })
 
   const renderSummary = () => {
     return div({ style: { margin: '4em' } }, [
       div({ style: { display: 'flex', marginTop: '1rem', justifyContent: 'space-between' } }, [
-        h2(['Fastq_to_ubam'])
+        h2([methodsData.name])
       ]),
       div({ style: { lineHeight: 2.0 } }, [
         div([
           span({ style: { fontWeight: 'bold' } }, ['Workflow source link: ']),
           a(
-            { href: wdlUrl },
-            [wdlUrl]
+            { href: methodsData.source_url },
+            [methodsData.source_url]
           )
         ]),
-        div([span({ style: { fontWeight: 'bold' } }, ['Version: ']), '1.14']),
+        div([span({ style: { fontWeight: 'bold' } }, ['Version: ']), '1.14 <TODO: WHERE DOES THIS COME FROM?>']),
         h(
           Link,
           {
@@ -39,11 +71,11 @@ export const SubmissionConfig = () => {
         isDisabled: false,
         'aria-label': 'Select a data table',
         isClearable: false,
-        value: dataTable.name,
-        onChange: ({ value }) => setDatatable({ name: value }),
+        value: selectedTable.name,
+        onChange: ({ value }) => setSelectedTable({ name: value }),
         placeholder: 'None selected',
-        styles: { container: old => ({ ...old, display: 'inline-block', width: 200, marginLeft: '0.5rem' }) },
-        options: ['Covid19_DataTable', 'Some Other DataTable']
+        styles: { container: old => ({ ...old, display: 'inline-block', width: 200 }) },
+        options: _.map(d => d.name, dataTables)
       }),
       h(StepButtons, {
         tabs: [
@@ -76,13 +108,31 @@ export const SubmissionConfig = () => {
   }
 
   return h(Fragment, [
-    headerBar(),
-    renderSummary(),
-    Utils.switchCase(activeTab.key || 'select-data',
-      ['select-data', () => renderDataSelector()],
-      ['inputs', () => renderInputs()],
-      ['outputs', () => renderOutputs()]
-    )
+    div({
+      style: {
+        borderBottom: '2px solid rgb(116, 174, 67)',
+        boxShadow: 'rgb(0 0 0 / 26%) 0px 2px 5px 0px, rgb(0 0 0 / 16%) 0px 2px 10px 0px',
+        position: 'relative'
+      }
+    }, [
+      headerBar(),
+      renderSummary()
+    ]),
+    div({
+      style: {
+        backgroundColor: 'rgb(235, 236, 238)',
+        display: 'flex',
+        flex: '1 1 auto',
+        flexDirection: 'column',
+        padding: '1rem 3rem'
+      }
+    }, [
+      Utils.switchCase(activeTab.key || 'select-data',
+        ['select-data', () => renderDataSelector()],
+        ['inputs', () => renderInputs()],
+        ['outputs', () => renderOutputs()]
+      )
+    ])
   ])
 }
 
