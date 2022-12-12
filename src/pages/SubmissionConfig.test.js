bbededcf0a0a0ece300843f1631dcdee845b28e0
Michael Spector <michael.l.spector@gmail.com>
2022-12-12 11:23:43 -0500

    fix runSets response; fix lots of other things

--- src/pages/SubmissionConfig.test.js
+++ src/pages/SubmissionConfig.test.js
@@ -1,11 +1,10 @@
 import '@testing-library/jest-dom'
 
-import { act, fireEvent, render, screen, within, waitFor } from '@testing-library/react'
+import { act, render, screen, waitFor, within } from '@testing-library/react'
 import { h } from 'react-hyperscript-helpers'
+import selectEvent from 'react-select-event'
 import { Ajax } from 'src/libs/ajax'
 import { SubmissionConfig } from 'src/pages/SubmissionConfig'
-import selectEvent from 'react-select-event'
-
 
 
 jest.mock('src/libs/ajax')
@@ -18,24 +17,21 @@ jest.mock('src/libs/config', () => ({
 }))
 
 const runSetResponse = {
-  runs: [
+  run_sets: [
     {
-      run_id: 'ea001565-1cd6-4e43-b446-932ac1918081',
-      record_id: '00000000-0000-0000-0000-000000000001',
+      run_set_id: '10000000-0000-0000-0000-000000000001',
+      method_id: '00000000-0000-0000-0000-000000000001',
+      is_template: true,
+      run_set_name: 'Target workflow 1, run 1',
+      run_set_description: 'Example run for target workflow 1',
       state: 'COMPLETE',
-      submission_date: '2022-11-23 15:03:28.202094',
-      workflow_url: 'https://abc.wdl',
-      last_modified_timestamp: '2022-11-23 15:04:15.359591',
-      workflow_params: '[{"parameter_name":"workflow_input_foo","parameter_type":"String","source":{"type":"literal","entity_attribute":"helloworld"}},{"parameter_name":"workflow_input_foo_rating","parameter_type":"Int","source":{"type":"entity_lookup","entity_attribute":"entity_field_foo_rating"}}]'
-    },
-    {
-      run_id: 'b7234aae-6f43-405e-bb3a-71f924e09825',
-      record_id: '00000000-0000-0000-0000-000000000002',
-      state: 'SYSTEM_ERROR',
-      submission_date: '2022-07-14T22:22:15.591Z',
-      workflow_url: 'https://xyz.wdl',
-      last_modified_timestamp: '2022-07-14T23:14:25.791Z',
-      workflow_params: '[{"parameter_name":"workflow_input_foo","parameter_type":"String","source":{"type":"literal","entity_attribute":"helloworld"}},{"parameter_name":"workflow_input_foo_rating","parameter_type":"Int","source":{"type":"entity_lookup","entity_attribute":"entity_field_foo_rating"}}]'
+      record_type: 'FOO',
+      submission_timestamp: '2022-12-07T17:26:53.153+00:00',
+      last_modified_timestamp: '2022-12-07T17:26:53.153+00:00',
+      run_count: 0,
+      error_count: 0,
+      input_definition: '[\n  {\n    "input_name": "target_workflow_1.foo.input_file_1",\n    "input_type": { "type": "primitive", "primitive_type": "File" },\n    "source": {\n      "type": "record_lookup",\n      "record_attribute": "target_workflow_1_input_file_1"\n    }\n  },\n  {\n    "input_name": "target_workflow_1.foo.input_file_2",\n    "input_type": { "type": "primitive", "primitive_type": "File" },\n    "source": {\n      "type": "record_lookup",\n      "record_attribute": "target_workflow_1_input_file_2"\n    }\n  },\n  {\n    "input_name": "target_workflow_1.foo.input_string_1",\n    "input_type": { "type": "primitive", "primitive_type": "String" },\n    "source": {\n      "type": "record_lookup",\n      "record_attribute": "target_workflow_1_input_string_1"\n    }\n  },\n  {\n    "input_name": "target_workflow_1.foo.input_string_2",\n    "input_type": { "type": "primitive", "primitive_type": "String" },\n    "source": {\n      "type": "record_lookup",\n      "record_attribute": "target_workflow_1_input_string_2"\n    }\n  },\n  {\n    "input_name": "target_workflow_1.foo.input_string_3",\n    "input_type": { "type": "optional", "optional_type": { "type": "primitive", "primitive_type": "String" } },\n    "source": {\n      "type": "record_lookup",\n      "record_attribute": "target_workflow_1_input_string_3"\n    }\n  },\n  {\n    "input_name": "target_workflow_1.foo.input_string_4",\n    "input_type": { "type": "primitive", "primitive_type": "String" },\n    "source": {\n      "type": "record_lookup",\n      "record_attribute": "target_workflow_1_input_string_4"\n    }\n  },\n  {\n    "input_name": "target_workflow_1.foo.input_string_5",\n    "input_type": { "type": "primitive", "primitive_type": "String" },\n    "source": {\n      "type": "record_lookup",\n      "record_attribute": "target_workflow_1_input_string_5"\n    }\n  },\n  {\n    "input_name": "target_workflow_1.foo.input_string_6",\n    "input_type": { "type": "primitive", "primitive_type": "String" },\n    "source": {\n      "type": "record_lookup",\n      "record_attribute": "target_workflow_1_input_string_6"\n    }\n  },\n  {\n    "input_name": "target_workflow_1.foo.input_string_7",\n    "input_type": { "type": "primitive", "primitive_type": "String" },\n    "source": {\n      "type": "record_lookup",\n      "record_attribute": "target_workflow_1_input_string_7"\n    }\n  }\n]\n',
+      output_definition: '[\n  {\n    "output_name": "target_workflow_1.file_output",\n    "output_type": { "type": "primitive", "primitive_type": "String" },\n    "record_attribute": "target_workflow_1_file_output"\n  }\n]\n'
     }
   ]
 }
@@ -68,34 +64,70 @@ const typesResponse = [
     ],
     count: 4,
     primaryKey: 'sys_name'
+  },
+  {
+    name: 'BAR',
+    attributes: [
+      {
+        name: 'bar_rating',
+        datatype: 'NUMBER'
+      },
+      {
+        name: 'sys_name',
+        datatype: 'STRING'
+      }
+    ],
+    count: 4,
+    primaryKey: 'sys_name'
   }
 ]
 
-const searchResponse = { 
-  searchRequest: { 
-    limit: 10, 
-    offset: 0, 
-    sort: 'ASC', 
-    sortAttribute: null 
-  }, 
+const searchResponseFOO = {
+  searchRequest: {
+    limit: 10,
+    offset: 0,
+    sort: 'ASC',
+    sortAttribute: null
+  },
   records: [
-    { 
-      id: 'FOO1', type: 'FOO', attributes: { sys_name: 'FOO1', foo_rating: 1000 } 
-    }, 
-    { 
-      id: 'FOO2', type: 'FOO', attributes: { sys_name: 'FOO2', foo_rating: 999 } 
-    }, 
-    { 
-      id: 'FOO3', type: 'FOO', attributes: { sys_name: 'FOO3', foo_rating: 85 } 
-    }, 
-    { 
-      id: 'FOO4', type: 'FOO', attributes: { sys_name: 'FOO4', foo_rating: 30 } 
+    {
+      id: 'FOO1', type: 'FOO', attributes: { sys_name: 'FOO1', foo_rating: 1000 }
+    },
+    {
+      id: 'FOO2', type: 'FOO', attributes: { sys_name: 'FOO2', foo_rating: 999 }
+    },
+    {
+      id: 'FOO3', type: 'FOO', attributes: { sys_name: 'FOO3', foo_rating: 85 }
+    },
+    {
+      id: 'FOO4', type: 'FOO', attributes: { sys_name: 'FOO4', foo_rating: 30 }
     }
-  ], 
-  totalRecords: 4 
+  ],
+  totalRecords: 4
 }
 
-const submissionId = 'e8347247-4738-4ad1-a591-56c119f93f58'
+const searchResponseBAR = {
+  searchRequest: {
+    limit: 10,
+    offset: 0,
+    sort: 'ASC',
+    sortAttribute: null
+  },
+  records: [
+    {
+      id: 'BAR1', type: 'BAR', attributes: { sys_name: 'BAR1', bar_rating: 1000 }
+    },
+    {
+      id: 'BAR2', type: 'BAR', attributes: { sys_name: 'BAR2', bar_rating: 999 }
+    }
+  ],
+  totalRecords: 2
+}
+
+const searchResponses = {
+  FOO: searchResponseFOO,
+  BAR: searchResponseBAR
+}
 
 describe('SubmissionConfig records selector', () => {
   // SubmissionConfig component uses AutoSizer to determine the right size for table to be displayed. As a result we need to
@@ -123,7 +155,7 @@ describe('SubmissionConfig records selector', () => {
     // Arrange
     const mockRunSetResponse = jest.fn(() => Promise.resolve(runSetResponse))
     const mockMethodsResponse = jest.fn(() => Promise.resolve(methodsResponse))
-    const mockSearchResponse = jest.fn(() => Promise.resolve(searchResponse))
+    const mockSearchResponse = jest.fn(recordType => Promise.resolve(searchResponses[recordType]))
     const mockTypesResponse = jest.fn(() => Promise.resolve(typesResponse))
 
     await Ajax.mockImplementation(() => {
@@ -147,21 +179,22 @@ describe('SubmissionConfig records selector', () => {
       }
     })
 
-    // Needed: a way to wait for initialization to be done
-    
-    // don't use "act" unless we see warning spam from the test
-    // "act" is a way of telling the test that some async things may be happening
-    const { getByTestId, getByLabelText } = render(h(SubmissionConfig))
+    render(h(SubmissionConfig))
 
     await waitFor(() => {
       expect(mockRunSetResponse).toHaveBeenCalledTimes(1)
       expect(mockMethodsResponse).toHaveBeenCalledTimes(1)
       expect(mockTypesResponse).toHaveBeenCalledTimes(1)
-      expect(mockSearchResponse).toHaveBeenCalledTimes(1)
+      expect(mockSearchResponse).toHaveBeenCalledTimes(0)
     })
 
     const table = await screen.findByRole('table')
-    
+
+    // after the initial render (not before), records data should have been retrieved once
+    await waitFor(() => {
+      expect(mockSearchResponse).toHaveBeenCalledTimes(1)
+    })
+
     const rows = within(table).queryAllByRole('row')
     expect(rows.length).toBe(5)
 
@@ -171,12 +204,11 @@ describe('SubmissionConfig records selector', () => {
     const cells = within(rows[1]).queryAllByRole('cell')
     expect(cells.length).toBe(4)
   })
-
-  it('should rpopulate the record selector when the dropdown selection changes', async () => {
+  it('should repopulate the record selector when the dropdown selection changes', async () => {
     // Arrange
     const mockRunSetResponse = jest.fn(() => Promise.resolve(runSetResponse))
     const mockMethodsResponse = jest.fn(() => Promise.resolve(methodsResponse))
-    const mockSearchResponse = jest.fn(() => Promise.resolve(searchResponse))
+    const mockSearchResponse = jest.fn(recordType => Promise.resolve(searchResponses[recordType]))
     const mockTypesResponse = jest.fn(() => Promise.resolve(typesResponse))
 
     await Ajax.mockImplementation(() => {
@@ -201,36 +233,54 @@ describe('SubmissionConfig records selector', () => {
     })
 
     // Needed: a way to wait for initialization to be done
-    
+
     // don't use "act" unless we see warning spam from the test
     // "act" is a way of telling the test that some async things may be happening
-    const { getByTestId, getByLabelText } = render(h(SubmissionConfig))
-
-
-    // render(h(SubmissionConfig))
-    // findByText, queryByText, ... (there's a cheat sheet) ... all expect to be awaited. (there is a default timeout)
-    const dropdown = await screen.findByRole('combobox')
-    
-    await act(async () => {
-      await selectEvent.select(dropdown, ['FOO'])
-    })
+    render(h(SubmissionConfig))
 
     await waitFor(() => {
       expect(mockRunSetResponse).toHaveBeenCalledTimes(1)
       expect(mockMethodsResponse).toHaveBeenCalledTimes(1)
       expect(mockTypesResponse).toHaveBeenCalledTimes(1)
-      expect(mockSearchResponse).toHaveBeenCalledTimes(1)
+      expect(mockSearchResponse).toHaveBeenCalledTimes(0)
     })
 
     const table = await screen.findByRole('table')
-    
-    const rows = within(table).queryAllByRole('row')
-    expect(rows.length).toBe(5)
 
-    const headers = within(rows[0]).queryAllByRole('columnheader')
+    // after the initial render (not before), records data should have been retrieved once
+    await waitFor(() => {
+      expect(mockSearchResponse).toHaveBeenCalledTimes(1)
+    })
+
+    const dropdown = await screen.findByRole('combobox')
+    await act(async () => {
+      await selectEvent.select(dropdown, ['BAR'])
+    })
+
+    // selecting a dropdown option should trigger a re-render, and a second call to records data
+    await waitFor(() => {
+      expect(mockSearchResponse).toHaveBeenCalledTimes(2)
+    })
+
+    const rowsBAR = within(table).queryAllByRole('row')
+    expect(rowsBAR.length).toBe(3)
+
+    const headers = within(rowsBAR[0]).queryAllByRole('columnheader')
     expect(headers.length).toBe(4)
 
-    const cells = within(rows[1]).queryAllByRole('cell')
+    const cells = within(rowsBAR[1]).queryAllByRole('cell')
     expect(cells.length).toBe(4)
+
+    await act(async () => {
+      await selectEvent.select(dropdown, ['FOO'])
+    })
+
+    // selecting a dropdown option should (again) trigger a re-render, and a third call to records data
+    await waitFor(() => {
+      expect(mockSearchResponse).toHaveBeenCalledTimes(3)
+    })
+
+    const rowsFOO = within(table).queryAllByRole('row')
+    expect(rowsFOO.length).toBe(5)
   })
 })
