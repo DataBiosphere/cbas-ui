aac73bda1f50715d61bc8e7d7dac5a847861f268
Katrina Pierre <kpierre@broadinstitute.org>
2022-12-13 15:24:49 -0500

    cleanup and eslint

--- src/pages/SubmissionDetails.test.js
+++ src/pages/SubmissionDetails.test.js
@@ -17,7 +17,7 @@ jest.mock('src/libs/config', () => ({
 
 describe('Submission Details page', () => {
   // SubmissionDetails component uses AutoSizer to determine the right size for table to be displayed. As a result we need to
-  // mock out the height and width so that when AutoSizer asks for the width and height of "browser" it can use the mocked
+  // mock out the height and width so that when AutoSizer asks for the width and height of 'browser' it can use the mocked
   // values and render the component properly. Without this the tests will be break.
   // (see https://github.com/bvaughn/react-virtualized/issues/493 and https://stackoverflow.com/a/62214834)
   const originalOffsetHeight = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'offsetHeight')
@@ -27,27 +27,27 @@ describe('Submission Details page', () => {
     runs: [
       {
         run_id: 'b7234aae-6f43-405e-bb3a-71f924e09825',
-        engine_id: "b29e84b1-ad1b-4462-a9a0-7ec849bf30a8",
-        run_set_id: "0cd15673-7342-4cfa-883d-819660184a16",
-        record_id: "FOO2",
+        engine_id: 'b29e84b1-ad1b-4462-a9a0-7ec849bf30a8',
+        run_set_id: '0cd15673-7342-4cfa-883d-819660184a16',
+        record_id: 'FOO2',
         workflow_url: 'https://xyz.wdl',
         state: 'SYSTEM_ERROR',
-        workflow_params: "[{\"input_name\":\"wf_hello.hello.addressee\",\"input_type\":{\"type\":\"primitive\",\"primitive_type\":\"String\"},\"source\":{\"type\":\"record_lookup\",\"record_attribute\":\"foo_name\"}}]",
-        workflow_outputs: "[]",
+        workflow_params: '[{\'input_name\':\'wf_hello.hello.addressee\',\'input_type\':{\'type\':\'primitive\',\'primitive_type\':\'String\'},\'source\':{\'type\':\'record_lookup\',\'record_attribute\':\'foo_name\'}}]',
+        workflow_outputs: '[]',
         submission_date: '2022-07-14T22:22:15.591Z',
         last_modified_timestamp: '2022-07-14T23:14:25.791Z',
       },
       {
-        run_id: "55b36a53-2ff3-41d0-adc4-abc08aea88ad",
-        engine_id: "d16721eb-8745-4aa2-b71e-9ade2d6575aa",
-        run_set_id: "0cd15673-7342-4cfa-883d-819660184a16",
-        record_id: "FOO1",
-        workflow_url: "https://raw.githubusercontent.com/broadinstitute/cromwell/a40de672c565c4bbd40f57ff96d4ee520dc2b4fc/centaur/src/main/resources/standardTestCases/hello/hello.wdl",
-        state: "COMPLETE",
-        workflow_params: "[{\"input_name\":\"wf_hello.hello.addressee\",\"input_type\":{\"type\":\"primitive\",\"primitive_type\":\"String\"},\"source\":{\"type\":\"record_lookup\",\"record_attribute\":\"foo_name\"}}]",
-        workflow_outputs: "[]",
-        submission_date: "2022-12-08T23:29:18.675+00:00",
-        last_modified_timestamp: "2022-12-08T23:29:55.695+00:00"
+        run_id: '55b36a53-2ff3-41d0-adc4-abc08aea88ad',
+        engine_id: 'd16721eb-8745-4aa2-b71e-9ade2d6575aa',
+        run_set_id: '0cd15673-7342-4cfa-883d-819660184a16',
+        record_id: 'FOO1',
+        workflow_url: 'https://raw.githubusercontent.com/broadinstitute/cromwell/a40de672c565c4bbd40f57ff96d4ee520dc2b4fc/centaur/src/main/resources/standardTestCases/hello/hello.wdl',
+        state: 'COMPLETE',
+        workflow_params: '[{\'input_name\':\'wf_hello.hello.addressee\',\'input_type\':{\'type\':\'primitive\',\'primitive_type\':\'String\'},\'source\':{\'type\':\'record_lookup\',\'record_attribute\':\'foo_name\'}}]',
+        workflow_outputs: '[]',
+        submission_date: '2022-12-08T23:29:18.675+00:00',
+        last_modified_timestamp: '2022-12-08T23:29:55.695+00:00'
       }
     ]
   }
@@ -55,19 +55,19 @@ describe('Submission Details page', () => {
   const runSetData = {
     run_sets: [
       {
-        run_set_id: "e8347247-4738-4ad1-a591-56c119f93f58",
-        method_id: "00000000-0000-0000-0000-000000000004",
+        run_set_id: 'e8347247-4738-4ad1-a591-56c119f93f58',
+        method_id: '00000000-0000-0000-0000-000000000004',
         is_template: false,
-        run_set_name: "hello world",
-        run_set_description: "test",
-        state: "COMPLETE",
-        record_type: "FOO",
-        submission_timestamp: "2022-01-01T12:00:00.000+00:00",
-        last_modified_timestamp: "2022-01-02T13:01:01.000+00:00",
+        run_set_name: 'hello world',
+        run_set_description: 'test',
+        state: 'COMPLETE',
+        record_type: 'FOO',
+        submission_timestamp: '2022-01-01T12:00:00.000+00:00',
+        last_modified_timestamp: '2022-01-02T13:01:01.000+00:00',
         run_count: 1,
         error_count: 0,
-        input_definition: "[{\"input_name\":\"wf_hello.hello.addressee\",\"input_type\":{\"type\":\"primitive\",\"primitive_type\":\"String\"},\"source\":{\"type\":\"record_lookup\",\"record_attribute\":\"foo_name\"}}]",
-        output_definition: "[]"
+        input_definition: '[{\'input_name\':\'wf_hello.hello.addressee\',\'input_type\':{\'type\':\'primitive\',\'primitive_type\':\'String\'},\'source\':{\'type\':\'record_lookup\',\'record_attribute\':\'foo_name\'}}]',
+        output_definition: '[]'
       }
     ]
   }
@@ -75,12 +75,12 @@ describe('Submission Details page', () => {
   const methodData = {
     methods: [
       {
-        method_id: "00000000-0000-0000-0000-000000000004",
-        name: "Hello world",
-        description: "Add description",
-        source: "Github",
-        source_url: "https://raw.githubusercontent.com/broadinstitute/cromwell/a40de672c565c4bbd40f57ff96d4ee520dc2b4fc/centaur/src/main/resources/standardTestCases/hello/hello.wdl",
-        created: "2022-12-08T23:28:50.280+00:00"
+        method_id: '00000000-0000-0000-0000-000000000004',
+        name: 'Hello world',
+        description: 'Add description',
+        source: 'Github',
+        source_url: 'https://raw.githubusercontent.com/broadinstitute/cromwell/a40de672c565c4bbd40f57ff96d4ee520dc2b4fc/centaur/src/main/resources/standardTestCases/hello/hello.wdl',
+        created: '2022-12-08T23:28:50.280+00:00'
       }
     ]
   }
@@ -152,13 +152,11 @@ describe('Submission Details page', () => {
     within(cellsFromDataRow2[3]).getByText('55b36a53-2ff3-41d0-adc4-abc08aea88ad')
   })
 
-  it('should display standard message when there are no saved workflows', async () => {
-
+  it('should display standard message when there are no saved workflows', () => {
     // Act
-    render(h(SubmissionDetails, { submissionId }))
-    // await act(async () => {
-    //   await render(h(SubmissionDetails, { submissionId }))
-    // })
+    act(async () => {
+      await render(h(SubmissionDetails, { submissionId }))
+    })
     const table = screen.getByRole('table')
 
     // Assert
@@ -203,7 +201,6 @@ describe('Submission Details page', () => {
   })
 
   it('display run set details', async () => {
-
     const getRunsSets = jest.fn(() => Promise.resolve(runSetData))
     const getMethods = jest.fn(() => Promise.resolve(methodData))
     await Ajax.mockImplementation(() => {
