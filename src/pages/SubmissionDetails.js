7724fb4d3d458c8472404ee6997be74d86e86029
Katrina Pierre <kpierre@broadinstitute.org>
2022-12-14 15:40:23 -0500

    functioning error filter (not cleaned up)

--- src/pages/SubmissionDetails.js
+++ src/pages/SubmissionDetails.js
@@ -42,15 +42,22 @@ export const SubmissionDetails = ({ submissionId }) => {
       Utils.differenceFromNowInSeconds(submitted)
   }
 
-  useOnMount(() => {
-    const loadRunsData = async () => {
-      try {
-        const runs = await Ajax(signal).Cbas.runs.get(submissionId)
-        setRunsData(runs.runs)
-      } catch (error) {
-        notify('error', 'Error loading previous runs', { detail: await (error instanceof Response ? error.text() : error) })
+  const loadRunsData = async filter => {
+    try {
+      const runs = await Ajax(signal).Cbas.runs.get(submissionId)
+      const allRuns = runs.runs
+      if (filter) {
+        setRunsData(_.filter(r => r.error_messages, allRuns))
+      } else {
+        setRunsData(allRuns)
       }
+    } catch (error) {
+      notify('error', 'Error loading previous runs', { detail: await (error instanceof Response ? error.text() : error) })
     }
+  }
+
+  useOnMount(() => {
+
 
     const loadRunSetData = async () => {
       try {
