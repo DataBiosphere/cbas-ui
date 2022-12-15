bf3d4532266d504fb1b480e605bcc501603f873e
Katrina Pierre <kpierre@broadinstitute.org>
2022-12-15 16:56:31 -0500

    linter

--- src/pages/SubmissionDetails.js
+++ src/pages/SubmissionDetails.js
@@ -172,7 +172,7 @@ export const SubmissionDetails = ({ submissionId }) => {
                   const failureStates = ['SYSTEM_ERROR', 'EXECUTOR_ERROR']
                   if (failureStates.includes(paginatedPreviousRuns[rowIndex].state)) {
                     return div({ style: { width: '100%', textAlign: 'center' } }, [
-                      h(Link, { key: 'error link', style: { fontWeight: 'bold' }, onClick: () => setViewErrorsId(rowIndex) }, [[icon('warning-standard', { key: 'error',size: 18, style: { color: colors.danger() } })], ['      Error(s)']])
+                      h(Link, { key: 'error link', style: { fontWeight: 'bold' }, onClick: () => setViewErrorsId(rowIndex) }, [[icon('warning-standard', { key: 'error', size: 18, style: { color: colors.danger() } })], ['      Error(s)']])
                     ])
                   } else if (paginatedPreviousRuns[rowIndex].state === 'COMPLETE') {
                     return div({ style: { width: '100%', textAlign: 'center' } }, [
