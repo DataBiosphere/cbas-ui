f4ac36e3284b986e3ec414e97a08df1181b522a7
Katrina Pierre <kpierre@broadinstitute.org>
2022-12-15 12:21:07 -0500

    additional terminal state

--- src/pages/SubmissionDetails.js
+++ src/pages/SubmissionDetails.js
@@ -30,7 +30,7 @@ export const SubmissionDetails = ({ submissionId }) => {
 
   const signal = useCancellation()
 
-  const terminalStates = ['ERROR', 'COMPLETE']
+  const terminalStates = ['ERROR', 'COMPLETE', 'CANCELED']
   const duration = ({
     state,
     submission_timestamp: submitted,
