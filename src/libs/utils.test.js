1d7829156bda15ba2fa27e3ac18ce793ad21e86b
mspector <michael.l.spector@gmail.com>
2023-01-23 09:13:54 -0500

    [WM-1652] move function to utils; handle potentially nested array_type (#78)

--- src/libs/utils.test.js
+++ src/libs/utils.test.js
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 
-import { makeCompleteDate } from 'src/libs/utils'
+import { makeCompleteDate, renderTypeText } from 'src/libs/utils'
 
 
 describe('makeCompleteDate', () => {
@@ -8,5 +8,17 @@ describe('makeCompleteDate', () => {
     // since the timestamp is being converted to Local timezone, it returns different time when run locally and in GitHub action.
     // Hence verifying only the date format for now.
     expect(makeCompleteDate('2022-01-27T22:27:15.591Z').toString()).toContain('Jan 27, 2022')
+  })
+})
+
+describe('submission-common tests', () => {
+  it('variable type text is rendered properly', () => {
+    expect(renderTypeText({ type: 'primitive', primitive_type: 'File' })).toStrictEqual('File')
+    expect(renderTypeText({ type: 'optional', optional_type: { type: 'primitive', primitive_type: 'String' } })).toStrictEqual('String (optional)')
+    expect(renderTypeText({ type: 'array', array_type: { type: 'primitive', primitive_type: 'Int' } })).toStrictEqual('Array[Int]')
+    expect(renderTypeText({ type: 'array', array_type: { type: 'optional', optional_type: { type: 'primitive', primitive_type: 'Int' } } })).toStrictEqual('Array[Int (optional)]')
+    expect(renderTypeText({ type: 'array', array_type: { type: 'array', array_type: { type: 'array', array_type: { type: 'primitive', primitive_type: 'Int' } } } })).toStrictEqual('Array[Array[Array[Int]]]')
+    expect(renderTypeText({ type: 'struct', struct_type: 'File' })).toStrictEqual('Unsupported Type')
+    expect(renderTypeText({ type: 'map', map_type: 'File' })).toStrictEqual('Unsupported Type')
   })
 })
