1c5806a8f1e2c1bce8279c33772582316dd23359
Katrina Pierre <kpierre@broadinstitute.org>
2023-03-03 14:33:25 -0500

    Added Optimus workflow; added POST methods endpoint

--- src/pages/FindWorkflow/FindWorkflowModal.js
+++ src/pages/FindWorkflow/FindWorkflowModal.js
@@ -4,7 +4,10 @@ import { div, h } from 'react-hyperscript-helpers'
 import { Clickable, Link } from 'src/components/common'
 import { icon } from 'src/components/icons'
 import ModalDrawer from 'src/components/ModalDrawer'
+import { Ajax } from 'src/libs/ajax'
 import colors from 'src/libs/colors'
+import * as Nav from 'src/libs/nav'
+import { notify } from 'src/libs/notifications'
 import * as Style from 'src/libs/style'
 import { MethodCard } from 'src/pages/FindWorkflow/MethodCard'
 
@@ -22,11 +25,11 @@ const styles = {
 
 const suggestedWorkflowsList = [
   {
-    method_name: 'mock_method_1',
-    method_description: 'mock_method_1 description. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    method_name: 'Optimus',
+    method_description: 'The optimus 3 pipeline processes 10x genomics sequencing data based on the v2 chemistry. It corrects cell barcodes and UMIs, aligns reads, marks duplicates, and returns data as alignments in BAM format and as counts in sparse matrix exchange format.',
     method_source: 'GitHub',
-    method_version: 'master',
-    method_url: 'https://raw.githubusercontent.com/broadinstitute/mock_method_1.wdl'
+    method_version: '1.0',
+    method_url: 'https://raw.githubusercontent.com/broadinstitute/warp/develop/pipelines/skylab/optimus/Optimus.wdl'
   },
   {
     method_name: 'mock_method_2',
@@ -43,6 +46,31 @@ const suggestedWorkflowsList = [
     method_url: 'https://raw.githubusercontent.com/broadinstitute/mock_method_3.wdl'
   }
 ]
+
+const submitMethod = async () => {
+  try {
+    const methodPayload = {
+      run_set_name: runSetName,
+      run_set_description: runSetDescription,
+      method_version_id: selectedMethodVersion.method_version_id,
+      workflow_input_definitions: configuredInputDefinition,
+      workflow_output_definitions: configuredOutputDefinition,
+      wds_records: {
+        record_type: selectedRecordType,
+        record_ids: _.keys(selectedRecords)
+      }
+    }
+
+    setDisplayLaunchModal(false)
+    const methodObject = await Ajax(signal).Cbas.runSets.post(methodPayload)
+    notify('success', 'Workflow successfully submitted', { message: 'You may check on the progress of workflow on this page anytime.', timeout: 5000 })
+    Nav.goToPath('submission-details', {
+      submissionId: runSetObject.run_set_id
+    })
+  } catch (error) {
+    notify('error', 'Error submitting workflow', { detail: await (error instanceof Response ? error.text() : error) })
+  }
+}
 
 
 const FindWorkflowModal = ({ onDismiss }) => {
@@ -85,7 +113,7 @@ const FindWorkflowModal = ({ onDismiss }) => {
       ]),
       isSubHeaderActive('browse-suggested-workflows') && div({ style: { overflowY: 'auto', flexGrow: 1, display: 'flex', flexDirection: 'column', paddingLeft: '20px' } }, [
         div({ style: { display: 'flex', flexWrap: 'wrap', overflowY: 'auto', paddingBottom: 5, paddingLeft: 5 } }, [
-          _.map(method => h(MethodCard, { method, onClick: () => console.log(`Clicked on ${method.method_name} card`), key: method.method_name }), suggestedWorkflowsList)
+          _.map(method => h(MethodCard, { method, onClick: () => submitMethod(), key: method.method_name }), suggestedWorkflowsList)
         ])
       ])
     ])
