43d357688b57237aeb98a7124bd50c4732e01e5c
mspector <michael.l.spector@gmail.com>
2023-03-20 17:08:23 -0400

    [WM-1746] add two remaining WARP methods that require structs (#133)

--- src/pages/FindWorkflow/FindWorkflowModal.js
+++ src/pages/FindWorkflow/FindWorkflowModal.js
@@ -60,6 +60,20 @@ const suggestedWorkflowsList = [
     method_source: 'GitHub',
     method_version: 'scATAC 1.2.0',
     method_url: 'https://raw.githubusercontent.com/broadinstitute/warp/develop/pipelines/skylab/scATAC/scATAC.wdl'
+  },
+  {
+    method_name: 'WholeGenomeGermlineSingleSample',
+    method_description: 'Processes germline whole genome sequencing data.',
+    method_source: 'GitHub',
+    method_version: 'WholeGenomeGermlineSingleSample_v3.1.6',
+    method_url: 'https://raw.githubusercontent.com/broadinstitute/warp/WholeGenomeGermlineSingleSample_v3.1.6/pipelines/broad/dna_seq/germline/single_sample/wgs/WholeGenomeGermlineSingleSample.wdl'
+  },
+  {
+    method_name: 'ExomeGermlineSingleSample ',
+    method_description: 'Processes germline exome/targeted sequencing data',
+    method_source: 'GitHub',
+    method_version: 'ExomeGermlineSingleSample_v3.0.0',
+    method_url: 'https://raw.githubusercontent.com/broadinstitute/warp/ExomeGermlineSingleSample_v3.0.0/pipelines/broad/dna_seq/germline/single_sample/exome/ExomeGermlineSingleSample.wdl'
   }
 ]
 
