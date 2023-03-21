7a04f5d35f0a681edb695547bda361c9f4aa3c13
Michael Spector <michael.l.spector@gmail.com>
2023-03-21 17:07:27 -0400

    rename structBuilderPath to structIndexPath

--- src/components/StructBuilder.js
+++ src/components/StructBuilder.js
@@ -9,15 +9,15 @@ import { FlexTable, HeaderCell, TextCell } from 'src/components/table'
 import * as Utils from 'src/libs/utils'
 
 
-const buildStructTypePath = path => _.join('.', _.map(row => `fields.${row}.field_type`, path))
-const buildStructSourcePath = path => _.join('.', _.map(row => `fields.${row}.source`, path))
-const buildStructNamePath = path => _.replace(/\.field_type$/, '.field_name', buildStructTypePath(path))
+const buildStructTypePath = indexPath => _.join('.', _.map(row => `fields.${row}.field_type`, indexPath))
+const buildStructSourcePath = indexPath => _.join('.', _.map(row => `fields.${row}.source`, indexPath))
+const buildStructNamePath = indexPath => _.replace(/\.field_type$/, '.field_name', buildStructTypePath(indexPath))
 
-export const buildStructBreadcrumbs = (path, structType) => _.map(
-  // map slices of the path (e.g. [0, 1, 2] -> [0], [0, 1], [0, 1, 2])
+export const buildStructBreadcrumbs = (indexPath, structType) => _.map(
+  // map slices of the indexPath (e.g. [0, 1, 2] -> [0], [0, 1], [0, 1, 2])
   // onto their corresponding field_names within structType, via buildStructNamePath
-  end => _.get(buildStructNamePath(_.slice(0, end + 1, path)), structType),
-  _.range(0, path.length)
+  end => _.get(buildStructNamePath(_.slice(0, end + 1, indexPath)), structType),
+  _.range(0, indexPath.length)
 )
 
 export const StructBuilder = props => {
@@ -29,18 +29,18 @@ export const StructBuilder = props => {
     dataTableAttributes
   } = props
 
-  const [structBuilderPath, setStructBuilderPath] = useState([])
+  const [structIndexPath, setStructIndexPath] = useState([])
 
-  const structTypePath = buildStructTypePath(structBuilderPath)
-  const structSourcePath = buildStructSourcePath(structBuilderPath)
-  const structNamePath = buildStructNamePath(structBuilderPath)
+  const structTypePath = buildStructTypePath(structIndexPath)
+  const structSourcePath = buildStructSourcePath(structIndexPath)
+  const structNamePath = buildStructNamePath(structIndexPath)
 
   const currentStructName = structNamePath ? _.get(structNamePath, structType) : structName
   const currentStructType = structTypePath ? _.get(structTypePath, structType) : structType
   const currentStructSource = structSourcePath ? _.get(structSourcePath, structSource) : structSource
   const setCurrentStructSource = structSourcePath ? source => setStructSource(_.set(structSourcePath, source, structSource)) : setStructSource
 
-  const currentStructBreadcrumbs = buildStructBreadcrumbs(structBuilderPath, structType)
+  const currentStructBreadcrumbs = buildStructBreadcrumbs(structIndexPath, structType)
 
   const breadcrumbsHeight = 35
   return h(div, { 'aria-label': 'struct-breadcrumbs', style: { height: 500 } }, [
@@ -54,7 +54,7 @@ export const StructBuilder = props => {
     }, [
       h(TextCell, {}, [
         ..._.map(([i, name]) => h(Link, {
-          onClick: () => setStructBuilderPath(_.slice(0, i, structBuilderPath))
+          onClick: () => setStructIndexPath(_.slice(0, i, structIndexPath))
         }, `${name} / `), _.toPairs(_.initial([structName, ...currentStructBreadcrumbs]))),
         currentStructName
       ])
@@ -118,7 +118,7 @@ export const StructBuilder = props => {
                     updateSource: setInnerStructSource
                   })],
                 ['record_lookup', () => RecordLookupSelect({ source: innerStructSource, dataTableAttributes, updateSource: setInnerStructSource })],
-                ['object_builder', () => StructBuilderLink({ onClick: () => setStructBuilderPath([...structBuilderPath, rowIndex]) })],
+                ['object_builder', () => StructBuilderLink({ onClick: () => setStructIndexPath([...structIndexPath, rowIndex]) })],
                 ['none', () => h(TextCell, { style: { fontStyle: 'italic' } }, ['Optional'])]
               )
             }
