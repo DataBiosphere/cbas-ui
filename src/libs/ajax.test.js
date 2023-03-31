364a7855122fb14f811b58af8f667ce2437119be
Michael Spector <michael.l.spector@gmail.com>
2023-03-31 15:23:59 -0400

    fix all zero uuid

--- src/libs/ajax.test.js
+++ src/libs/ajax.test.js
@@ -31,15 +31,15 @@ const cbasPact = new PactV3({
 })
 
 describe('Ajax tests', () => {
-  it('should get back a run set with id 00000000-00000000-00000000-00000000', async () => {
+  it('should get back a run set with id 00000000-0000-0000-0000-000000000000', async () => {
     // ARRANGE
     const mockedResponses = {
-      'GET runs?run_set_id=00000000-00000000-00000000-00000000': {
+      'GET runs?run_set_id=00000000-0000-0000-0000-000000000000': {
         runs: [
           {
             run_id: regex(UUID_REGEX, '6ad0568c-61be-4339-b458-89bca258316f'),
             engine_id: regex(UUID_REGEX, '637ce42e-5390-49bb-a6a8-1775b8d62980'),
-            run_set_id: regex(UUID_REGEX, '00000000-00000000-00000000-00000000'),
+            run_set_id: regex(UUID_REGEX, '00000000-0000-0000-0000-000000000000'),
             record_id: string('ERR4868270'),
             workflow_url: regex('https://.*', 'https://raw.githubusercontent.com/broadinstitute/viral-pipelines/master/pipes/WDL/workflows/fetch_sra_to_bam.wdl'),
             state: regex('RUNNING|COMPLETE', 'RUNNING'),
@@ -53,9 +53,9 @@ describe('Ajax tests', () => {
     }
     await cbasPact.addInteraction({
       states: [{ description: 'at_least_one_run_exists' }],
-      uponReceiving: 'get run set with ID 00000000-00000000-00000000-00000000',
-      withRequest: { method: 'GET', path: '/api/batch/v1/runs', query: { run_set_id: '00000000-00000000-00000000-00000000' } },
-      willRespondWith: { status: 200, body: mockedResponses['GET runs?run_set_id=00000000-00000000-00000000-00000000'] }
+      uponReceiving: 'get run set with ID 00000000-0000-0000-0000-000000000000',
+      withRequest: { method: 'GET', path: '/api/batch/v1/runs', query: { run_set_id: '00000000-0000-0000-0000-000000000000' } },
+      willRespondWith: { status: 200, body: mockedResponses['GET runs?run_set_id=00000000-0000-0000-0000-000000000000'] }
     })
 
     await cbasPact.executeTest(async mockService => {
@@ -63,15 +63,15 @@ describe('Ajax tests', () => {
       fetchCbas.mockImplementation(async path => await fetch(`${mockService.url}/api/batch/v1/${path}`))
 
       // ACT
-      const response = await Ajax('fakeSignal').Cbas.runs.get('00000000-00000000-00000000-00000000')
+      const response = await Ajax('fakeSignal').Cbas.runs.get('00000000-0000-0000-0000-000000000000')
 
       // ASSERT
       expect(response).toBeDefined()
       expect(fetchCbas).toBeCalledTimes(1)
-      expect(fetchCbas).toBeCalledWith('runs?run_set_id=00000000-00000000-00000000-00000000', { method: 'GET', signal: 'fakeSignal' })
+      expect(fetchCbas).toBeCalledWith('runs?run_set_id=00000000-0000-0000-0000-000000000000', { method: 'GET', signal: 'fakeSignal' })
       expect(response).toHaveProperty('runs')
       expect(response.runs.length).toEqual(1)
-      expect(response.runs[0]).toMatchObject({ run_set_id: '00000000-00000000-00000000-00000000' })
+      expect(response.runs[0]).toMatchObject({ run_set_id: '00000000-0000-0000-0000-000000000000' })
     })
   })
 })
