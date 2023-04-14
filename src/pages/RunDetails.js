271c8a11ff399d0c211dc4241f355004a9803296
Tom Wiseman <twiseman@broadinstitute.org>
2023-04-14 12:29:22 -0400

    remove unnecessary brackets

--- src/pages/RunDetails.js
+++ src/pages/RunDetails.js
@@ -210,9 +210,7 @@ export const RunDetails = ({ submissionId, workflowId }) => {
                   {
                     style: { marginBottom: '1rem' },
                     initialOpenState: true,
-                    title: div({ style: elements.sectionHeader }, [
-                      'Workflow-Level Failures'
-                    ]),
+                    title: div({ style: elements.sectionHeader }, 'Workflow-Level Failures'),
                     afterTitle: h(ClipboardButton, {
                       text: JSON.stringify(failures, null, 2),
                       style: { marginLeft: '0.5rem' },
