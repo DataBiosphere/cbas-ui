2b2633ebabb2c25662b024e1f9e00d4ac552bea5
Chris Llanwarne <cjllanwarne@gmail.com>
2023-05-03 15:20:51 -0400

    lint again

--- src/libs/ajax.test.js
+++ src/libs/ajax.test.js
@@ -42,19 +42,19 @@ const cbasPact = new PactV3({
 })
 
 describe('Leo Tokens', () => {
-  it('should get LeoToken from a one-cookie string',  () => {
+  it('should get LeoToken from a one-cookie string', () => {
     const input = 'LeoToken=1234'
     const output = extractLeoTokenFromCookies(input)
     expect(output).toEqual('1234')
   })
 
-  it('should get LeoToken from a multi-cookie string without spaces',  () => {
+  it('should get LeoToken from a multi-cookie string without spaces', () => {
     const input = '_ga=foo;_ga_bar=baz;LeoToken=1234'
     const output = extractLeoTokenFromCookies(input)
     expect(output).toEqual('1234')
   })
 
-  it('should get LeoToken from a multi-cookie string with spaces',  () => {
+  it('should get LeoToken from a multi-cookie string with spaces', () => {
     const input = '_ga=foo; _ga_bar=baz; LeoToken=1234'
     const output = extractLeoTokenFromCookies(input)
     expect(output).toEqual('1234')
