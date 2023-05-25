77a66584d77e38687ef80ac9709caaf77ed692ce
Katrina Pierre <kpierre@broadinstitute.org>
2023-05-25 15:21:43 -0400

    wip

--- src/libs/ajax.test.js
+++ src/libs/ajax.test.js
@@ -25,7 +25,8 @@ const {
   regex,
   boolean,
   integer,
-  fromProviderState
+  fromProviderState,
+  valueFromProviderState
 } = MatchersV3
 
 const UUID_REGEX = '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
