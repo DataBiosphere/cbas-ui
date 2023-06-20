122c9b4652ec360a8883386bb3ca2ee3f6b7eca1
Saloni Shah <salonipshah11@gmail.com>
2023-06-20 17:48:34 -0400

    add tests for other types

--- src/components/RecordsTable.test.js
+++ src/components/RecordsTable.test.js
@@ -16,6 +16,9 @@ describe('RecordsTable', () => {
         foo_int: 123,
         foo_float: 12.34,
         foo_date: '2023-06-16',
+        foo_file: 'https://datasettoaexample.blob.core.windows.net/dataset/abc.genome.fa',
+        foo_array_of_string: ["this","is","a","string","array"],
+        foo_array_of_files: ["https://datasettoaexample.blob.core.windows.net/dataset/abc.fastq.gz","https://datasettoaexample.blob.core.windows.net/dataset/xyz.fastq.gz"],
         foo_struct: { foo_tries: 3, agg_foo_tries: 3, nested_foo_struct: { foo_rating: 4.5, bar_rating: 2.4 } }
       }
     }
@@ -43,6 +46,18 @@ describe('RecordsTable', () => {
       {
         name: 'foo_date',
         datatype: 'DATE'
+      },
+      {
+        name: 'foo_file',
+        datatype: 'FILE'
+      },
+      {
+        name: 'foo_array_of_string',
+        datatype: 'ARRAY_OF_STRING'
+      },
+      {
+        name: 'foo_array_of_files',
+        datatype: 'ARRAY_OF_FILE'
       },
       {
         name: 'foo_struct',
@@ -93,6 +108,9 @@ describe('RecordsTable', () => {
     within(dataRow[4]).getByText('123')
     within(dataRow[5]).getByText('12.34')
     within(dataRow[6]).getByText('2023-06-16')
-    within(dataRow[7]).getByText('{"foo_tries":3,"agg_foo_tries":3,"nested_foo_struct":{"foo_rating":4.5,"bar_rating":2.4}}')
+    within(dataRow[7]).getByText('https://datasettoaexample.blob.core.windows.net/dataset/abc.genome.fa')
+    within(dataRow[8]).getByText('["this","is","a","string","array"]')
+    within(dataRow[9]).getByText('["https://datasettoaexample.blob.core.windows.net/dataset/abc.fastq.gz","https://datasettoaexample.blob.core.windows.net/dataset/xyz.fastq.gz"]')
+    within(dataRow[10]).getByText('{"foo_tries":3,"agg_foo_tries":3,"nested_foo_struct":{"foo_rating":4.5,"bar_rating":2.4}}')
   })
 })
