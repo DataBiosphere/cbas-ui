c3aca41a8c2e6437e569a302632b992ce9a1df6e
Saloni Shah <salonipshah11@gmail.com>
2023-06-30 16:43:21 -0400

    linter issues

--- src/libs/ajax.test.js
+++ src/libs/ajax.test.js
@@ -335,7 +335,7 @@ describe('Ajax tests', () => {
       states: [
         { description: 'ready to fetch myMethodVersion with UUID 90000000-0000-0000-0000-000000000009' },
         { description: 'cromwell initialized' },
-        { description: 'ready to receive exactly 1 call to POST /method endpoint' },
+        { description: 'ready to receive exactly 1 call to POST /method endpoint' }
       ],
       uponReceiving: 'a POST request to import a method',
       withRequest: { method: 'POST', path: '/api/batch/v1/methods', body, headers },
